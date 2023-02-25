0628869eb80e165a7f65cf07287d68ed3cd9bead
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-02-24 23:40:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,15 @@ num_documento = st.text_input("Ingrese el número de documento a consultar", '')
 df = df[(df['Mes'] == mes) & (df['Quincena'] == quincena) & (df['Num_Documento'] == num_documento)]
 data = df.loc[:, ['Proceso', 'Aprobadas', 'Valor_Unidad', 'Valor_Total']]
 
-st.dataframe(data.style.hide_index())
+# CSS to inject contained in a string
+hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+
+st.dataframe(data)
