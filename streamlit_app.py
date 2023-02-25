fa627fb563cd24ae7fe90879b928077221417fca
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-02-24 22:48:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,13 @@ client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 sql = """
-    SELECT * 
-    FROM `digitales-373718.covmaritex.Calidad`
+    SELECT E.Num_Documento, C.*, P.Costo AS Valor_Unidad, (C.Aprobadas * P.Costo) AS Valor_Total, EXTRACT(MONTH FROM C.Fecha) AS Mes,
+    CASE WHEN EXTRACT(DAY FROM C.Fecha) <=15 THEN 'Q1' ELSE 'Q2' END AS Quincena,
+    FROM `digitales-373718.covmaritex.Calidad` AS C
+    JOIN covmaritex.Procesos AS P
+    ON C.Mos = P.Mos AND C.Proceso = P.Proceso
+    JOIN covmaritex.Empleados AS E
+    ON C.Manual = E.Nombre_Completo
     """
 
 df = client.query(sql).to_dataframe()
