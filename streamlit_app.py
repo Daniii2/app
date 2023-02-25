46495cac85388e9f092871721bac5cdec60d2fe2
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-02-24 22:57:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 sql = """
-    SELECT E.Num_Documento, C.*, P.Costo AS Valor_Unidad, (C.Aprobadas * P.Costo) AS Valor_Total, EXTRACT(MONTH FROM C.Fecha) AS Mes,
+    SELECT E.Num_Documento, C.*, ROUND(P.Costo, 0) AS Valor_Unidad, ROUND((C.Aprobadas * P.Costo), 0) AS Valor_Total, EXTRACT(MONTH FROM C.Fecha) AS Mes,
     CASE WHEN EXTRACT(DAY FROM C.Fecha) <=15 THEN 'Q1' ELSE 'Q2' END AS Quincena,
     FROM `digitales-373718.covmaritex.Calidad` AS C
     JOIN covmaritex.Procesos AS P
