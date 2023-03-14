7cb295ea5da1f1f930cb52020250fb1a56622528
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-03-14 16:07:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ with col3:
 c_path = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSv0ShIcmXQ9-mN_Vr6sZGZExejKYTqs2C22iMGbCwqbI2L_g9G2I4oyAphLmGQG3DM4I75nsc0o3OK/pub?gid=0&single=true&output=csv'
 p_path = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1b55YBjl3Nx619-ZxcA1HFI4I-HKNEkj1e5mF4Ou_44Zttfjn0huUCPluDRdxoeMEZAZVJgU-AqFF/pub?gid=0&single=true&output=csv'
 m_path = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vScaT6bvHkLPpetxUalJpZiM-1CF4fH3iKEw0oM4LiE7bhOuOQZ0fvTr20yD7qHYH3mLtgtghoDGyrj/pub?gid=0&single=true&output=csv'
+l_path = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS38H6Hh28X9Xto3k1_TDV4h8GYp5xpYjvCk6Esl4ksp8PdjSFJdBoiP34j30g5-5tJr2upfXFnAGQb/pub?gid=0&single=true&output=csv'
 
 #Calidad dataframe
 calidad = pd.read_csv(c_path, sep=',', header=0)
@@ -29,12 +30,15 @@ calidad['IDProceso'] = calidad.Mos+calidad.Proceso
 #Procesos
 procesos = pd.read_csv(p_path, sep=',', header=0)
 procesos['IDProceso'] = procesos['IDProceso'].astype('string')
-#Manuakes
+#Manuales
 manuales = pd.read_csv(m_path, sep=',', header=0)
 manuales['Nombre_Completo'] = manuales['Nombres']+' '+manuales['Apellidos']
+#Llegadas
+llegadas = pd.read_csv(l_path, sep=',', header=0)
 #Merge
 calidad_p = calidad.merge(procesos, how='left', left_on=['IDProceso'], right_on=['IDProceso'])
-df = calidad_p.merge(manuales, how='left', left_on=['Manual'], right_on=['Nombre_Completo'])
+calidad_pm = calidad_p.merge(manuales, how='left', left_on=['Manual'], right_on=['Nombre_Completo'])
+df = calidad_pm.merge(llegadas, how='left', left_on=['Mos'], right_on=['Mos'])
 #Transformations
 
 df['Num_Documento'] = df['Num_Documento'].astype('string')
@@ -58,7 +62,7 @@ st.write('Ingresaste:', num_documento)
 
 #Applying filters to dataframes
 df = df[(df['Mes'] == mes) & (df['Quincena'] == quincena) & (df['Num_Documento'] == num_documento)]
-data = df.loc[:, ['Proceso_x', 'Aprobadas', 'Costo', 'Valor_Total']].rename(columns={'Proceso_x':'Proceso', 'Costo':'Valor_Unidad'})
+data = df.loc[:, ['Proceso_x', 'Referencia', 'Aprobadas', 'Costo', 'Valor_Total']].rename(columns={'Proceso_x':'Proceso', 'Costo':'Valor_Unidad'})
 
 col1, col2 = st.columns(2)
 with col1:
