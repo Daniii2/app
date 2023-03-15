c2a45f0c4f55f7ff6cafd216054f78847a85e6b7
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-03-15 13:26:50 -0500

    Update manuales.py

--- pages/manuales.py
+++ pages/manuales.py
@@ -89,7 +89,7 @@ st.write('Ingresaste:', num_documento)
 #Applying filters to dataframes
 data = df[(df['Mes'] == mes) & (df['Quincena'] == quincena) & (df['Num_Documento'] == num_documento)]
 data = data.set_index('Num_documento')
-data = data.loc[:, ['asignados', 'recibidos', 'pendientes', 'aprobadas', 'devueltas']]
+#data = data.loc[:, ['asignados', 'recibidos', 'pendientes', 'aprobadas', 'devueltas']]
 
 
 st.dataframe(data)
