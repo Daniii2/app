f886bec5eaa17d156da9353716cc9db2b5a6996e
dataeng-mr <126300720+dataeng-mr@users.noreply.github.com>
2023-03-14 23:03:39 -0500

    Update Manuales.py

--- pages/Manuales.py
+++ pages/Manuales.py
@@ -34,37 +34,14 @@ llegadas['Mos'] = llegadas['Mos'].astype('string')
 recibidos = pd.read_csv(r_path, sep=',', header=0)
 recibidos['Mos'] = recibidos['Mos'].astype('int')
 recibidos['Mos'] = recibidos['Mos'].astype('string')
+recibidos['BK'] = recibidos['Manual']+recibidos['Mos']+recibidos['Talla']
 #Asignaciomes
 asignaciones = pd.read_csv(a_path, sep=',', header=0)
 asignaciones['Mos'] = asignaciones['Mos'].astype('int')
 asignaciones['Mos'] = asignaciones['Mos'].astype('string')
+asignaciones['BK'] = asignaciones['Manual']+asignaciones['Mos']+asignaciones['Talla']
 #Merge
-
-#Mes filter
-mes = st.selectbox(
-    'Seleccione un mes',
-    list(set(df.Mes)))
-st.write('Seleccionaste:', mes)
-
-#Quincena filter
-quincena = st.selectbox(
-    'Seleccione una quincena',
-    list(set(df.Quincena)))
-st.write('Seleccionaste:', quincena)
-
-#Cedula text filter
-num_documento = st.text_input("Ingrese el número de documento a consultar", '')
-st.write('Ingresaste:', num_documento)
-
-#Applying filters to dataframes
-df = df[(df['Mes'] == mes) & (df['Quincena'] == quincena) & (df['Num_Documento'] == num_documento)]
-data = df.loc[:, ['Proceso_x', 'Referencia', 'Aprobadas', 'Costo', 'Valor_Total']].rename(columns={'Proceso_x':'Proceso', 'Costo':'Valor_Unidad'})
-
-col1, col2 = st.columns(2)
-with col1:
-    st.metric(label='Aprobadas', value=data['Aprobadas'].sum().astype('int'))
-with col2:
-    st.metric(label='Valor_Total', value=data['Valor_Total'].sum().astype('int'))
+data = asignaciones.merge(recibidos, how='left', on=['BK'])
 
 st.dataframe(data)
 
